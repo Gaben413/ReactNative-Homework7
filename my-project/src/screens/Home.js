1fe31149e75f14289c3205ab95112f3f30246e3c
NowhereCat <gabrielbportela413@gmail.com>
2023-05-04 16:20:20 -0300

    Update Home.js

--- my-project/src/screens/Home.js
+++ my-project/src/screens/Home.js
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export function Home(){
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
+            <Button title="CHECK" />
         </View>
     )
 }
